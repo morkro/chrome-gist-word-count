099c1d09e4fc7c2014300835f1e4bd7e2a823140
Moritz Kröger <write@morkro.de>
2016-04-05 15:22:28 +0200

    Updated logic and improved README

--- README.md
+++ README.md
@@ -1,9 +1,17 @@
-# Gist Count
+# Gist Word Count
 
 Adds a word count to your files on GitHub Gist.
 
-Supported languages:
+![Screenshot of word count](screenshot.png)
+
+## Install
+Install the extension via Chrome Web Store, or [download](https://github.com/morkro/chrome-gist-word-count/archive/master.zip) this repository and add it as unpacked Chrome extension.
+
+### Supported languages:
 - Markdown
 - Text
 
 Excludes Markdown code blocks which begin and end with three backtick characters.
+
+## License :copyright:
+The code is available under [MIT License](https://github.com/morkro/DOMtimer/blob/master/LICENSE).
--- index.js
+++ index.js
@@ -1,65 +1,63 @@
 'use strict';
 
-/**
- * Configuration
+/*
+ * =========================================================================== *
+ * CONFIGURATION
+ * =========================================================================== *
  */
-const SUPPORTED = ['Markdown', 'Text'];
+const SUPPORTED = [
+	'Markdown',
+	'Text'
+];
 const REGEX = {
 	clearWhitespace: /\s+/gi,
 	removeMarkdownCode: /```(.*?)```/gm
 };
-const REQUEST = new Request(`https://api.github.com/gists/${getGistHashFromUrl()}`, {
-	method: 'GET',
-	headers: new Headers({
-		'If-Modified-Since': localStorage.getItem('gist-ext-last-modified') || ''
-	})
-});
 
-/**
- * Initialise GitHub API
+/*
+ * =========================================================================== *
+ * ALL THE FUNCTIONALITY
+ * =========================================================================== *
  */
-fetch(REQUEST)
-	.then(response => {
-		if (response.status !== 404) {
-			localStorage.setItem('gist-ext-last-modified', response.headers.get('Last-Modified'));
+function isGistOverviewPage () {
+	return document.querySelectorAll('.file-box .file-header').length > 0;
+}
 
-			if (response.status === 200) {
-				return response.json();
-			}
-			else {
-				return JSON.parse(localStorage.getItem('gist-ext-last-storage'));
-			}
-		}
-	})
-	.then(json => {
-		if (json || json !== undefined) {
-			localStorage.setItem('gist-ext-last-storage', JSON.stringify(json));
-			return scanFiles(json.files);
-		}
-	})
-	.catch(error => console.warn(error));
-
-/**
- * Doing the magic
- */
-function getGistHashFromUrl () {
+function getGistIDFromUrl () {
 	const path = window.location.pathname.split('/');
 	return path[path.length - 1];
 }
 
+function requestGitHubAPI () {
+	const previous = JSON.parse(localStorage.getItem('gist-ext-last-storage'));
+	const config = { method: 'GET' };
+
+	if (previous && previous.id === getGistIDFromUrl()) {
+		config.headers = new Headers({
+			'If-Modified-Since': localStorage.getItem('gist-ext-last-modified')
+		});
+	}
+
+	return new Request(
+		`https://api.github.com/gists/${getGistIDFromUrl()}`,
+		config
+	);
+}
+
 function getWordCount (text, type) {
 	text = text.trim();
+	const { clearWhitespace, removeMarkdownCode } = REGEX;
 
 	if (type === 'words') {
 		return text
-			.replace(REGEX.clearWhitespace, ' ')
-			.replace(REGEX.removeMarkdownCode, '')
+			.replace(clearWhitespace, ' ')
+			.replace(removeMarkdownCode, '')
 			.split(' ').length;
 	}
 	else if (type === 'characters') {
 		return text
-			.replace(REGEX.clearWhitespace, '')
-			.replace(REGEX.removeMarkdownCode, '')
+			.replace(clearWhitespace, '')
+			.replace(removeMarkdownCode, '')
 			.length;
 	}
 	else return text.length;
@@ -84,7 +82,7 @@ function addWordCountInfo (file, index) {
 	const gistContent = document.querySelector(
 		`.gist-content .file-box:nth-of-type(${++index + 1})`
 	);
-	const gistElement = gistContent.querySelector('.file-info');;
+	const gistElement = gistContent.querySelector('.file-info');
 
 	gistElement.appendChild(wordCountElement);
 }
@@ -96,4 +94,32 @@ function scanFiles (files) {
 			addWordCountInfo(file, index);
 		}
 	});
+}
+
+/*
+ * =========================================================================== *
+ * INITIALISE EXTENSION
+ * =========================================================================== *
+ */
+if (isGistOverviewPage()) {
+	const previous = JSON.parse(localStorage.getItem('gist-ext-last-storage'));
+
+	fetch(requestGitHubAPI())
+		.then(response => {
+			if (response.status !== 404) {
+				localStorage.setItem(
+					'gist-ext-last-modified',
+					response.headers.get('Last-Modified')
+				);
+
+				return (response.status === 200) ? response.json() : previous;
+			}
+		})
+		.then(json => {
+			if (json) {
+				localStorage.setItem('gist-ext-last-storage', JSON.stringify(json));
+				return scanFiles(json.files);
+			}
+		})
+		.catch(error => console.warn(error));
 }
